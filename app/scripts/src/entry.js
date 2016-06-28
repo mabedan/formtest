30e7b399bbc40c86d0ec13ba2fe82a5621ee179b
Mehdi Esfahani <mehdiesfahani@mehdis-mbp.check24.intern>
2016-06-28 12:49:11 +0200

    add form editor

--- app/scripts/src/entry.js
+++ app/scripts/src/entry.js
@@ -2,18 +2,12 @@
 
 var options = require("./form/options.js");
 var Page = require("./form/components/page.js");
+var formMaker = require("./form/utils/formMaker.js");
 
 var firstPage = Page();
 firstPage.appendTo("body");
+var creatorPage;
 
-renderForm();
-
-function renderForm() {
-	firstPage.empty();
-	getLocalForm().forEach(function(item) {
-		Page.addSection(options[item](), firstPage);
-	});
-}
 
 function getDefaultForm () {
 	return [
@@ -29,7 +23,7 @@ function getDefaultForm () {
 }
 
 function getLocalForm () {
-	var local = localStorage.formItems
+	var local = localStorage.formItems;
 	if (local) {
 		return local.split(",");
 	} else {
@@ -38,3 +32,29 @@ function getLocalForm () {
 		return local;
 	}
 }
+
+function renderForm() {
+	firstPage.empty();
+	getLocalForm().forEach(function(item) {
+		Page.addSection(options[item](), firstPage);
+	});
+}
+
+$("body").append($("<button>").text("edit").css("color", "white").click(function () {
+	if (creatorPage) {
+		var itms = [];
+		creatorPage.find(".added-items").children().each(function () {
+			itms.push($(this).children().remove().end().text());
+		});
+		localStorage.formItems = itms;
+		creatorPage.remove();
+		creatorPage = undefined;
+		renderForm();
+	} else {
+		creatorPage = formMaker.creatorPage(getLocalForm());
+		$("body").append(creatorPage);
+	}
+}));
+
+renderForm();
+
--- app/scripts/src/form/utils/formMaker.js
+++ app/scripts/src/form/utils/formMaker.js
@@ -1,4 +1,44 @@
 "use strict";
-module.exports = {
+var Page = require("../components/page.js");
+var Options = require("../options.js");
+
+function createCreator(itms) {
+	var itemsList = $("<div>").addClass("added-items");
+	function item (name) {
+		var el = $("<div>").text(name)
+			.append($("<button>").text("up").click(function () {
+				el.insertBefore(el.prev());
+			}))
+			.append($("<button>").text("down").click(function () {
+				el.insertAfter(el.next());
+			}))
+			.append($("<button>").text("X").click(function () {
+				el.remove();
+			}));
+
+		return el;
+	}
+	function addItem(name) {
+		itemsList.append(item(name));
+	}
+	function choice (name) {
+		return $("<div>").text(name).click(function () {
+			addItem(name);
+		});
+	}
+	itms.forEach(addItem);
 	
+	return $("<div>").addClass("creator")
+		.append(itemsList)
+		.append($("<div>").addClass("options").append(_.map(Options, function(opt, key) {
+			return choice(key);
+		})));
+}
+
+module.exports = {
+	creatorPage: function (itms) {
+		var page = Page();
+		page.append(createCreator(itms));
+		return page;
+	}
 };
